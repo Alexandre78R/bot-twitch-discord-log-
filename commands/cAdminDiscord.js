5d20a3bb828339ec4f5222889523ed6cfed6ee7a
Alexandre78R <alexandre.renard98@gmail.com>
2019-08-29 22:34:17 +0200

    Ajout des commantaire sur les commandes.

--- commands/cAdminDiscord.js
+++ commands/cAdminDiscord.js
@@ -1,38 +1,57 @@
+//Import de la config
 const config = require('../config/config.json')
+
+//Import de la LIBS discord.js
 const Discord = require("discord.js");
 
+//Module export des commandes admins qui sont appeler dans le fichier clientDisocrd.js
 module.exports = {
     'aide_modo': aide_modo,
 } 
 
+//Commande aide_modo
 function aide_modo (message) {
 
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
    
+    //Prise en compte du préfix
     if (message.length == 1){
         if (message[0].charAt(0) == config.discord.prefix) 
             message[0] = message[0].slice(1);
 
     }
 
+    //Création de la réponse en cas si l'utilisateur n'a pas la permission de faire cette commande
     let aide_modoPerm = new Discord.RichEmbed()
     .setTitle("Réponse de la commande :")
     .setColor("#bc0000")
     .addField(":x: Tu n'as pas le droit de utilisé cette commande.", "👮 Bien essayer en tous cas.")
     .setFooter("( Auto-destruction du message dans 10s ! )")
+
+    //Suppression de la commande de l'utilisateur qui la taper.
     message.delete().catch(O_o=>{});
 
+    //On vérifie si l'utilisateur à la permission de faire la commande
     if (!message.channel.permissionsFor(message.author).hasPermission("MANAGE_MESSAGES")) {
+
+        //Envoie de la réponse erreur si il n'a pas la permission avec une auto-destruction de 10s
         message.channel.sendMessage(aide_modoPerm).then(message => {message.delete(6000)});
+
         console.log("Désolé, vous n'avez pas la permission d'exécuter la commande \""+message.content+"\"");
+        
         return;
     }
 
+    //Création de la réponse si l'utilisateur à bien la permission 
     let aideembed = new Discord.RichEmbed()
     .setColor("#15f153")
     .addField(config.discord.prefix+"aide_modo", "Voir les commandes du bot, pour la modération !")
     .setFooter("( Auto-destruction du message dans 30s ! )")
+
+    //Envois de la réponse avec une auto-destruction de la réponse dans 30s
     message.channel.send(aideembed).then(message => {message.delete(18000)});;
+
+    //Suppression de la commande de l'utilisateur qui la taper.
     message.delete().catch(O_o=>{});
 } 
--- commands/cDiscord.js
+++ commands/cDiscord.js
@@ -1,57 +1,79 @@
+//Import de la config
 const config = require('../config/config.json')
+
+//Import de la LIBS discord.js
 const Discord = require("discord.js");
 
+//Module export des commandes qui sont appeler dans le fichier clientDisocrd.js
 module.exports = {
     'aide': aide,
     'serverinfo' : serverinfo,
     'avatar' : avatar,
 } 
 
+//Commande aide
 function aide (message) {
-
+    
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
    
+    //Prise en compte du prefix
     if (message.length == 1){
         if (message[0].charAt(0) == config.discord.prefix) 
             message[0] = message[0].slice(1);
 
     }
 
+    //Création de la réponse
     let aideembed = new Discord.RichEmbed()
     .setColor("#15f153")
     .addField(config.discord.prefix+"aide", "Voir les commandes du bot.")
     .addField(config.discord.prefix+"serverinfo", "Indique les informations du serveur.")
     .addField(config.discord.prefix+"avatar", "Permettre de voir votre photo de profil et avoir le lien.")
+    
+    //Envois de la réponse
     message.channel.send(aideembed);
 } 
 
+//Commande serverinfo
 function serverinfo (message) {
 
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
 
+    //Prise en compte du prefix
     if (message.length == 1){
-        if (message[0].charAt(0) == config.prefix) 
+        if (message[0].charAt(0) == config.discord.prefix) 
         message[0] = message[0].slice(1);
     }
 
+    //Annalyse du nombre de bot qui est sur le serveur discord
     function bot(guild) {
+        //Stockage du nombre de bot
         let botCount = 0;
+        //Annalyse de tous les membres
         guild.members.forEach(member => { 
+            //Quand il trouve un bot on rajoute +1 dans le stockage
             if(member.user.bot) botCount++; 
         });
+        //On retourne le nombre de bot
         return botCount;
     }
 
+    //Annalyse du nombre de membre qui est sur le serveur discord
     function membre(guild) {
+        //Stockage du nombre de membre
         let memberCount = 0;
+        //Annalyse de tous les membres
         guild.members.forEach(member => {
+            //Quand il trouve un membre on rajoute +1 dans le stockage
             if(!member.user.bot) memberCount++; 
         });
+        //On retourne le nombre de membre 
         return memberCount;
     }
 
+    //Création de la réponse
     let embed = new Discord.RichEmbed()
     .setAuthor(`${message.guild.name} - Informations`, message.guild.iconURL)
     .setColor('#15f153') 
@@ -64,26 +86,31 @@ function serverinfo (message) {
     .setFooter('Serveur Discord créer le :')
     .setTimestamp(`${message.guild.createdAt}`)
 
+    //Envoie de la réponse
     message.channel.send(embed);
 }
-   
-function avatar (msg) {
+
+//Commande avatar
+function avatar (message) {
 
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
 
-    if (msg.length == 1) {
-        if (msg[0].charAt(0) == config.prefix)
-            msg[0] = msg[0].slice(1);
+    //Prise en compte du préfix
+    if (message.length == 1) {
+        if (message[0].charAt(0) == config.discord.prefix)
+            message[0] = message[0].slice(1);
     }
 
+    //Création de la réponse
     let avatarEmbed = new Discord.RichEmbed()
     .setTitle(`Ton image de profil.`)
     .setColor("#15f153")
-    .setImage(`${msg.author.avatarURL}`)
-    .setURL(`${msg.author.avatarURL}`)
-    .setDescription(`[Lien direct vers l'image](${msg.author.avatarURL})`)
-    .setAuthor(`Par ${msg.author.username} - Informations`, msg.author.avatarURL);
+    .setImage(`${message.author.avatarURL}`)
+    .setURL(`${message.author.avatarURL}`)
+    .setDescription(`[Lien direct vers l'image](${message.author.avatarURL})`)
+    .setAuthor(`Par ${message.author.username} - Informations`, message.author.avatarURL);
 
-    msg.channel.send(avatarEmbed);
+    //Envoie de la réponse
+    message.channel.send(avatarEmbed);
 }
